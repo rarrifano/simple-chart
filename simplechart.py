612932e77d37664068f5ef3cbbbd408f520de844
Rafael <rafael.arrifano@protonmail.com>
2019-09-17 20:22:22 -0300

    Cleaning code

--- simplechart.py
+++ simplechart.py
@@ -11,7 +11,7 @@ from gi.repository import Gtk
 class FileChooserWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="FileChooser Example")
+        Gtk.Window.__init__(self, title="FileChooser")
         dialog = Gtk.FileChooserDialog("Please choose a file", self,
             Gtk.FileChooserAction.OPEN,
             (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
@@ -37,37 +37,16 @@ class FileChooserWindow(Gtk.Window):
             "Data de Nascimento", "Por que você prestou o Vestibular nesta faculdade?"], axis=1, inplace=True)
             coluna = data.columns
             plt.figure("Trabalho Sócio Econômico")
-            #testa a geração do gráfico de acordo com as colunas
+            # testa a geração do gráfico de acordo com as colunas
             for i in coluna:
                 lablel = data[i].value_counts()
                 data[i].value_counts().plot.pie(title= i, label=i, autopct='%1.1f%%',figsize=(16,9))
                 plt.axis('equal')
                 plt.tight_layout()
                 plt.legend(title=i,loc="best" )
-
                 plt.ylabel('')
-                
                 plt.show()
             
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
-
-    def on_file_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog("Please choose a file", self,
-            Gtk.FileChooserAction.OPEN,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-
-        self.add_filters(dialog)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Open clicked")
-            print("File selected: " + dialog.get_filename())
-            openx = dialog.get_filename()
-            print(openx)
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
 
